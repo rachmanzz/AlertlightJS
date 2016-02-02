202ea420adeeacdf9157b22bc5960ec5ce6e033b
Quesha-Lania <rachman.sd@gmail.com>
2016-02-02 09:54:18 +0800

    contributor : Rahman Za

--- dist/js/AlertlightJS.js
+++ dist/js/AlertlightJS.js
@@ -37,7 +37,35 @@ var alertlightJS = {
                     });
                 }
             }
-        )
+        );
+    },
+    $swalGet:function(swalOpt,postOpt,callback){
+        swal({
+                title: swalOpt['title'] || "title",
+                text: swalOpt['text'] || "text",
+                type: swalOpt['type'] || "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: swalOpt['confirmText'] || "Yes",
+                cancelButtonText: swalOpt['cancelText'] ||  "No",
+                closeOnConfirm: false,
+                closeOnCancel: false
+            },function(isConfirm){
+                if (isConfirm) {
+                    $.get(postOpt['url'],postOpt['input'],
+                        function(data,status){
+                            var value= jQuery.parseJSON(data);
+                            callback(true,{data:data,value:value,status:status},function(opt){
+                                swal(opt[0], opt[1], opt[2] || "success");
+                            });
+                        });
+                }else{
+                    callback(false,{},function(opt){
+                        swal(opt[0], opt[1], opt[2] || "error");
+                    });
+                }
+            }
+        );
     },
     swalPost:function(id,swalOpt,postOpt,callback){
         $(function(){
